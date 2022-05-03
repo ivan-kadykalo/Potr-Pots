a0702f1ae03f630752075d9d26483f727c86fcca
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-05-03 23:01:42 +0300

    Create landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Swiper
+const swiper = new Swiper('.swiper', { // eslint-disable-line
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+// Page scroll off
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#burger-menu') {
+    document.body.classList.add('page__body--scroll-off');
+  } else {
+    document.body.classList.remove('page__body--scroll-off');
+  }
+});
